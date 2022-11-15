c30bb0c42d25f7e077b2d184bfba3856b67a0b98
Jishnu-26 <jishnubalakrishnan26@gmail.com>
2022-11-15 07:23:25 +0530

    Hospital update

--- routes/students.js
+++ routes/students.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const bodyParser = require("body-parser")
 router.use(bodyParser.urlencoded({ extended: true }));
-const Student = require('../models/student')
+const Student = require('../models/ecard')
 // get students
 router.get('/',async (req, res) => { 
         try {
--- server.js
+++ server.js
@@ -1,21 +1,39 @@
-require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+app.set('view engine','ejs');
 const port = process.env.PORT ||  3000
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to db'))
 
+app.use(express.static("public"));
 
-app.use(express.json())
+app.get("/",function(req, res){
+    res.render("index");
+});
 
-const studentsRouter = require('./routes/students.js')
-// all endpoints with students are handled by student.js
-app.use('/students', studentsRouter)
+app.get("/login",function(req, res){
+  res.render("login");
+});
 
+app.get("/register",function(req, res){
+  res.render("register");
+});
 
+app.get("/tables",function(req, res){
+  res.render("tables");
+});
+
+app.get("/charts",function(req, res){
+  res.render("charts");
+});
+
+app.get("/forms",function(req, res){
+  res.render("forms");
+});
 
 app.listen(port, function(){
     console.log("Server is running");
