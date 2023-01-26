463fd6e0831303d06d8914a067266852b844f433
Jishnu-26 <jishnubalakrishnan26@gmail.com>
2023-01-27 01:39:11 +0530

    updated authentication and admin access

--- data.js
+++ data.js
@@ -1,3 +1,4 @@
+const { v4: uuidv4 } = require('uuid');
 const ROLE = {
     CHAIRMAN: 'chairman',
     MANAGER: 'manager',
@@ -5,8 +6,21 @@ const ROLE = {
 }
 module.exports = {
     users:[
-        {id:1, name:'Adam', role: ROLE.BASIC},
-        {id:2, name:'Steve', role: ROLE.CHAIRMAN},
-        {id:3, name:'Tom', role: ROLE.MANAGER}
+        {id:uuidv4(),name:'Adam', email:'adam@gmail', password:'hello', role: ROLE.BASIC},
+        {id:uuidv4(),name:'Steve',email:'steve@gmail', password:'nopenoway',role: ROLE.CHAIRMAN},
+        {id:uuidv4(),name:'Tom',email:'tom@gmail', password:'yayayaya',role: ROLE.MANAGER}
+    ],
+    managers:[
+        {id:uuidv4(),name:'Tom',exp:'5',branch:'Coimbatore','Age':25},
+        {id:uuidv4(),name:'Tory',exp:'7',branch:'Mumbai','Age':35},
+        {id:uuidv4(),name:'Sam',exp:'15',branch:'Bangalore','Age':45}
+
+    ],
+    userinfo:[
+        {id:uuidv4(),name:'Adam', ecard:'Lifelong', branch:'Bangalore',totalsave:'54'},
+        {id:uuidv4(),name:'Eleven', ecard:'Lifelong', branch:'Bangalore',totalsave:'500'},
+        {id:uuidv4(),name:'Max', ecard:'Twoyear', branch:'Coimbatore',totalsave:'800'},
+        {id:uuidv4(),name:'Wednesday', ecard:'Oneyear', branch:'Mumbai',totalsave:'0'},
+        {id:uuidv4(),name:'Robert', ecard:'Lifelong', branch:'Coimbatore',totalsave:'5000'},
     ]
 }
--- server.js
+++ server.js
@@ -1,28 +1,106 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const {users} = require('./data')
-const {authUser} = require('./basicauth')
 const bcrypt = require('bcrypt')
+const { v4: uuidv4 } = require('uuid');
+const { users } = require('./data')
+const { managers } = require('./data')
+const { userinfo } = require('./data')
+
+// console.log(users);
+Object.keys(users).forEach(key => console.log(users[key]));
 const app = express()
 app.set('view engine','ejs')
 const port = 3000
+app.use(bodyParser.urlencoded({extended: true}))
 
+flag = false
+// login
 app.get('/login', (req, res) => res.render("login"))
+app.post('/login',(req,res)=>{
+  console.log(req.body.email);
+  console.log(req.body.password);
+  for (var user of users) {
+    if(user.email==req.body.email && user.password==req.body.password){
+      flag=true
+      if(user.role=='basic'){
+        res.redirect("userhome")
+      }
+      else if(user.role=='chairman'){
+        res.redirect("chairmanhome")
+      }else{
+        console.log("logined as manager");
+        res.redirect("chairmanhome")
+      }
+    }
+}
+res.render("login")
+  
+})
+
+
+// register new user
 app.get('/register', (req,res)=>{
   res.render("register")
 })
-app.get('/chairmanhome', (req, res) => res.render("chairmanhome"))
-app.get('/userhome', authUser,(req, res) => res.render("userhome"))
-app.post('/register', (req, res) => {
-  bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
-    // Store hash in your password DB.
-});
+app.post('/register',(req,res)=>{
+  console.log(req.body.newemail);
+  console.log(req.body.newusername);
+  console.log(req.body.newpassword);
+  user = {}
+  user.id = uuidv4();
+  user.name=req.body.newusername
+  user.email=req.body.newemail
+  user.password=req.body.newpassword
+  user.role='basic'
+  users.push(user);
+  console.log(users);
 })
-function setUser(req,res,next){
-  const userId = req.body.userId
-  if(userId){
-    req.user= users.find(user=>user.id === userId)
+
+
+// chairman access
+app.get('/chairmanhome', (req, res) =>{ 
+  if(flag==false){
+    res.redirect("login")
+  }else{
+    res.render("chairmanhome")
+    flag=false
   }
-  next( )
-}
+  })
+
+app.post('/chairmanhome', (req,res)=>{
+  
+  // manager info
+  console.log(req.body.mid);
+  console.log(req.body.mname);
+  console.log(req.body.mexp);
+  console.log(req.body.mbranch);
+  console.log(req.body.mage);
+
+  // customer info
+  console.log(req.body.cid);
+  console.log(req.body.cname);
+  console.log(req.body.ecard);
+  console.log(req.body.cbranch);
+  console.log(req.body.csave);
+})
+
+
+// userdetails adding
+app.get('/userhome', (req, res) => {
+  if(flag==false){
+    res.redirect("login")
+  }else{
+    res.render("userhome")
+    flag=false
+  }
+})
+app.post('/userhome',(req,res)=>{
+  // customer info
+  console.log(req.body.cid);
+  console.log(req.body.cname);
+  console.log(req.body.ecard);
+  console.log(req.body.cbranch);
+  console.log(req.body.csave);  
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
