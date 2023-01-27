6c719a192fd324ce2462a78c20ec39fc868d6336
Jishnu-26 <jishnubalakrishnan26@gmail.com>
2023-01-27 22:37:10 +0530

    updated data.js for append retrive data

--- data.js
+++ data.js
@@ -1,19 +1,20 @@
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4, stringify } = require('uuid');
 const ROLE = {
     CHAIRMAN: 'chairman',
     MANAGER: 'manager',
     BASIC: 'basic'
 }
-module.exports = {
+
+module.exports   = {
     users:[
         {id:uuidv4(),name:'Adam', email:'adam@gmail', password:'hello', role: ROLE.BASIC},
         {id:uuidv4(),name:'Steve',email:'steve@gmail', password:'nopenoway',role: ROLE.CHAIRMAN},
         {id:uuidv4(),name:'Tom',email:'tom@gmail', password:'yayayaya',role: ROLE.MANAGER}
     ],
     managers:[
-        {id:uuidv4(),name:'Tom',exp:'5',branch:'Coimbatore','Age':25},
-        {id:uuidv4(),name:'Tory',exp:'7',branch:'Mumbai','Age':35},
-        {id:uuidv4(),name:'Sam',exp:'15',branch:'Bangalore','Age':45}
+        {id:uuidv4(),name:'Tom',exp:'5',branch:'Coimbatore',age:'25'},
+        {id:uuidv4(),name:'Tory',exp:'7',branch:'Mumbai',age:'35'},
+        {id:uuidv4(),name:'Sam',exp:'15',branch:'Bangalore',age:'45'}
 
     ],
     userinfo:[
--- server.js
+++ server.js
@@ -2,21 +2,25 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt')
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
 const { users } = require('./data')
 const { managers } = require('./data')
-const { userinfo } = require('./data')
-
+const { userinfo } = require('./data');
+const { userInfo } = require('os');
+// console.log(data.managers.push)
 // console.log(users);
 Object.keys(users).forEach(key => console.log(users[key]));
 const app = express()
 app.set('view engine','ejs')
 const port = 3000
 app.use(bodyParser.urlencoded({extended: true}))
-
+let useremail = ""
 flag = false
 // login
 app.get('/login', (req, res) => res.render("login"))
 app.post('/login',(req,res)=>{
+  useremail=req.body.email
   console.log(req.body.email);
   console.log(req.body.password);
   for (var user of users) {
@@ -53,7 +57,10 @@ app.post('/register',(req,res)=>{
   user.password=req.body.newpassword
   user.role='basic'
   users.push(user);
+  console.log(users)
   console.log(users);
+  res.redirect("login")
+
 })
 
 
@@ -62,35 +69,53 @@ app.get('/chairmanhome', (req, res) =>{
   if(flag==false){
     res.redirect("login")
   }else{
-    res.render("chairmanhome")
+    res.render("chairmanhome",{managers:managers , userinfo:userinfo})
     flag=false
   }
   })
 
 app.post('/chairmanhome', (req,res)=>{
   
-  // manager info
-  console.log(req.body.mid);
-  console.log(req.body.mname);
-  console.log(req.body.mexp);
-  console.log(req.body.mbranch);
-  console.log(req.body.mage);
+  manager = {}
+  if(req.body.mname!=null){
+  manager.id=uuidv4()
+  manager.name=req.body.mname
+  manager.exp=req.body.mexp
+  manager.age=req.body.mage
+  manager.branch=req.body.mbranch
+  managers.push(manager)
+  console.log(managers);
+  }
+  // res.render("chairmanhome")
+
 
   // customer info
-  console.log(req.body.cid);
-  console.log(req.body.cname);
-  console.log(req.body.ecard);
-  console.log(req.body.cbranch);
-  console.log(req.body.csave);
+  user = {}
+  if(req.body.cname!=null){
+  user.id=uuidv4()
+  user.name=req.body.cname
+  user.ecard=req.body.ecard
+  user.totalsave=req.body.csave
+  user.branch=req.body.cbranch
+  userinfo.push(user)
+  }
+  
 })
 
 
 // userdetails adding
 app.get('/userhome', (req, res) => {
+  username = ""
+  for (var user in users){
+    if (user.email==useremail){
+      username=user.name
+    }
+  }
+  var cursession = {email:useremail,name:username}
   if(flag==false){
     res.redirect("login")
   }else{
-    res.render("userhome")
+    res.render("userhome",{cursession:cursession})
     flag=false
   }
 })
